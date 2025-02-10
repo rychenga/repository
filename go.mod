33aa21c1c9b2924f092f173d60a820aa05e9ccdb
jeff-cheng <jeff.cheng@rapidsignage.com>
2025-02-10 16:44:55 +0800

    feat:PostgreSQL_v1

--- go.mod
+++ go.mod
@@ -3,13 +3,19 @@ module repository
 go 1.20
 
 require (
+	github.com/jackc/pgx/v5 v5.7.2
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.12
 )
 
 require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"embed"
 	"fmt"
 	"log"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -18,21 +20,25 @@ type SqlFiles struct {
 
 // MySQL 連線資訊
 const (
-	username = "root"
-	password = "Dev127336"
-	hostname = "127.0.0.1:3306"
-	dbname   = "demo"
+	mysqlusername = "root"
+	mysqlpassword = "Dev127336"
+	mysqlhostname = "127.0.0.1:3306"
+	mysqldbname   = "demo"
+	pgusername    = "admin"
+	pgpassword    = "pg123"
+	pghostname    = "127.0.0.1:5432"
+	pgdbname      = "demo"
 )
 
 // getDSN 返回 MySQL 連線字串
-func getDSN() string {
+func getMySqlDSN() string {
 	return fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		username, password, hostname, dbname)
+		mysqlusername, mysqlpassword, mysqlhostname, mysqldbname)
 }
 
 func QueryMySql(sqlQuery string) []map[string]interface{} {
 	// 連接 MySQL (GORM)
-	dsn := getDSN()
+	dsn := getMySqlDSN()
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("連線 MySQL 失敗: %v", err)
@@ -55,19 +61,62 @@ func QueryMySql(sqlQuery string) []map[string]interface{} {
 	return results
 }
 
+func QueryPostgresSQL(sqlQuery2 string) {
+	// 連線到 PostgreSQL
+	databaseUrl := "postgres://" + pgusername + ":" + pgpassword + "@" + pghostname + "/" + pgdbname
+	// fmt.Println(databaseUrl)
+
+	dbpool, err := pgxpool.New(context.Background(), databaseUrl)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+	defer dbpool.Close() //關掉連線
+
+	// 移除 SQL 結尾的分號（pgx 不接受帶分號的查詢）
+	sqlQuery2 = sqlQuery2[:len(sqlQuery2)-1]
+	// fmt.Println(sqlQuery2)
+
+	// 執行 SQL 查詢
+	var bookName = "D" // 替換成你的查詢條件
+	rows, err := dbpool.Query(context.Background(), sqlQuery2, bookName)
+	if err != nil {
+		log.Fatalf("Query PostgreSQL failed: %v\n", err)
+	}
+	defer rows.Close()
+	// 輸出查詢結果
+	fmt.Println("Query PostgreSQL Results:")
+	for rows.Next() {
+		var name string
+		var book_name string
+		if err := rows.Scan(&name, &book_name); err != nil {
+			log.Fatalf("Failed to scan row: %v\n", err)
+		}
+		fmt.Printf("Name: %s, Book Name: %s\n", name, book_name)
+	}
+}
+
 func main() {
 	fmt.Println("Hello, world!")
 	// 讀取sql file功能
 	s := SqlFiles{embedFiles: embedFiles}
-	f, err := s.embedFiles.ReadFile("sql/demo.sql")
+	f, err := s.embedFiles.ReadFile("sql/mysql_demo.sql")
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(f))
+	// fmt.Println(string(f))
 	sqlQuery := string(f)
 
 	//mysql 功能
 	r := QueryMySql(sqlQuery)
 	fmt.Println(r) //再一次顯示內容
+
+	s2 := SqlFiles{embedFiles: embedFiles}
+	f2, err := s2.embedFiles.ReadFile("sql/pg_demo.sql")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// fmt.Println(string(f2))
+	sqlQuery2 := string(f2)
+	QueryPostgresSQL(sqlQuery2) // 查詢PostgresSQL DB
 
 }
