d0361040318ac7b097f82fe79da0e0b81e63f528
jeff-cheng <jeff.cheng@rapidsignage.com>
2025-02-11 15:46:34 +0800

    feat:QueryMySql新增log功能

--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.20
 
 require (
 	github.com/jackc/pgx/v4 v4.18.3
+	go.uber.org/zap v1.21.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.25.12
+	moul.io/zapgorm2 v1.3.0
 )
 
 require (
@@ -23,6 +25,8 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 	"embed"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/jackc/pgx/v4/pgxpool"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	gormlogger "gorm.io/gorm/logger"
+	"moul.io/zapgorm2"
 )
 
 //go:embed **/*.sql
@@ -30,6 +35,26 @@ const (
 	pgdbname      = "demo"
 )
 
+// 記錄log用
+var logger *zap.Logger
+
+func initLogger() {
+	file, _ := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	writeSyncer := zapcore.AddSync(file)
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	core := zapcore.NewCore(
+		// zapcore.NewJSONEncoder(encoderConfig),
+		// writeSyncer,
+		zapcore.NewConsoleEncoder(encoderConfig),                             // 可能需要 ConsoleEncoder 而非 JSONEncoder
+		zapcore.NewMultiWriteSyncer(writeSyncer, zapcore.AddSync(os.Stdout)), // 確保寫入檔案 + 終端機
+		zap.InfoLevel,
+	)
+
+	logger = zap.New(core)
+	defer logger.Sync() // 保證在程式結束時同步
+}
+
 // getDSN 返回 MySQL 連線字串
 func getMySqlDSN() string {
 	return fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
@@ -43,13 +68,27 @@ func getPostgresDSN() string {
 }
 
 func QueryMySql(sqlQuery string) []map[string]interface{} {
+	// 創建 zapgorm2 日誌記錄器
+	// zapgormLogger := zapgorm2.New(logger)
+
+	//以下 zapgormLogger.Info 方法不能用
+	zapgormLogger := zapgorm2.New(logger).LogMode(gormlogger.Info)
+	// 使用 zapgormLogger 記錄 SQL 查詢
+	zapgormLogger.Info(context.Background(), "執行 SQL 查詢88", sqlQuery)
+	zapgormLogger.Info(context.TODO(), "執行 SQL 查詢99", sqlQuery)
+
 	// 連接 MySQL (GORM)
 	dsn := getMySqlDSN()
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: zapgormLogger, // 設定 GORM 日誌記錄器
+	})
 	if err != nil {
 		log.Fatalf("連線 MySQL 失敗: %v", err)
+		logger.Fatal("連線 MySQL 失敗:", zap.Error(err))
+		panic("failed to connect database")
 	}
-	fmt.Println("成功連線 MySQL!")
+	// fmt.Println("成功連線 MySQL!")
+	logger.Info("成功連線 MySQL!")
 
 	// 執行帶參數的 SQL 查詢
 	var results []map[string]interface{}
@@ -57,7 +96,9 @@ func QueryMySql(sqlQuery string) []map[string]interface{} {
 	result := db.Raw(sqlQuery, param).Scan(&results)
 	if result.Error != nil {
 		log.Fatalf("執行 SQL 查詢失敗: %v", result.Error)
+		logger.Fatal("執行 SQL 查詢失敗:", zap.Error(result.Error))
 	}
+	logger.Info("輸入 SQL 查詢", zap.String("SQL Commend: ", sqlQuery))
 
 	// 顯示查詢結果
 	fmt.Println("查詢結果:")
@@ -128,8 +169,12 @@ func main() {
 	sqlQuery := string(f)
 
 	//mysql 功能
+	initLogger()        // 初始化全域 Logger
+	defer logger.Sync() // 保證日誌寫入
 	r := QueryMySql(sqlQuery)
 	fmt.Println(r) //再一次顯示內容
+	logger.Info("執行 MySQL SQL 查詢", zap.String("SQL", sqlQuery))
+	// logger.Fatal("sql eroor conentx: ", zap.Error(err))
 
 	s2 := SqlFiles{embedFiles: embedFiles}
 	f2, err := s2.embedFiles.ReadFile("sql/pg_demo.sql")
